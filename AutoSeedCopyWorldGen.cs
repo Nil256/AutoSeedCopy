51a4030251cf8d986522344e6b1fd7403d1d3e2f
Nil256 <fnfennene256@gmail.com>
2025-08-18 15:39:00 +0900

    プロジェクト ファイルを追加します。

---
+++ AutoSeedCopyWorldGen.cs
@@ -0,0 +1,36 @@
+﻿using ReLogic.OS;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AutoSeedCopy
+{
+    public class AutoSeedCopyWorldGen : ModSystem
+    {
+        public override void PreWorldGen()
+        {
+            int worldSize = GetWorldSizeID(Main.maxTilesX, Main.maxTilesY);
+            int evilType = WorldGen.WorldGenParam_Evil + 1;
+            int gameMode = Main.GameMode + 1;
+            int seed = WorldGen._lastSeed;
+            string fullSeedsText = $"{worldSize}.{evilType}.{gameMode}.{seed}";
+            Platform.Get<IClipboard>().Value = fullSeedsText;
+        }
+
+        private static int GetWorldSizeID(in int worldSizeX, in int worldSizeY)
+        {
+            if (worldSizeX == 4200 && worldSizeY == 1200)
+            {
+                return 1;
+            }
+            if (worldSizeX == 6400 && worldSizeY == 1800)
+            {
+                return 2;
+            }
+            if (worldSizeX == 8400 && worldSizeY == 2400)
+            {
+                return 3;
+            }
+            return 0;
+        }
+    }
+}
